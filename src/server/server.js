b57fc5190df918f40a07117df96e506c5d7c68ff
Mihail Paley <undeadlol1@mail.ru>
2017-04-08 19:51:38 +0500

    add DB_PORT to test logs

--- src/server/server.js
+++ src/server/server.js
@@ -29,6 +29,7 @@ console.log('process.env.DB_USER', process.env.DB_USER)
 console.log('process.env.DB_PASS', process.env.DB_PASS)
 console.log('process.env.DB_NAME', process.env.DB_NAME)
 console.log('process.env.DB_HOST', process.env.DB_HOST)
+console.log('process.env.DB_PORT', process.env.DB_PORT)
 console.log('process.env.DB_DIALECT', process.env.DB_DIALECT)
 
 var Sequelize = require('sequelize');
