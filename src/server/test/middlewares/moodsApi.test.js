4a4112d8f42e1e1bed0b7215f1cba4fb9a46edf7
Mihail Paley <undeadlol1@mail.ru>
2017-04-12 15:07:07 +0500

    add basic testing environment with mocha-webpack

---
+++ src/server/test/middlewares/moodsApi.test.js
@@ -0,0 +1,33 @@
+var chai = require('chai')
+var request = require('supertest')
+
+chai.should();
+
+describe('/moods API CRUD', function() {
+    it('should GET moods', function() {
+        // request(app)
+            // .get('/api/moods')
+            // .expect('Content-Type', /json/)
+            // .expect('Content-Length', '15')
+            // .expect(200)
+            // .end(function(err, res) {
+            //     if (err) throw err;
+            // });
+    })
+
+    it('should GET single mood', function() {
+
+    })
+
+    it('should POST mood', function() {
+
+    })
+
+    it('should fail to POST if not authorized', function() { // TODO move this to previous function?
+
+    })
+
+    it('should GET /search moods', function() {
+
+    })
+})
---
+++ src/server/test/server.test.js
@@ -0,0 +1,10 @@
+// describe('app', function(){
+//   var app = require('../server.js');
+//   beforeEach(function(){
+//     app.listen(3000);
+//   });
+//   // tests here
+//   afterEach(function(){
+//     app.close();
+//   })
+// });
