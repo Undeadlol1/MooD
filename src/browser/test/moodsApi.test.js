7c845e4722c0caffcedff0cde5785d78457527f6
Mihail Paley <undeadlol1@mail.ru>
2017-05-04 09:45:07 +0500

    add boilerplate for auth tests

--- src/browser/test/moodsApi.test.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai'
-
-chai.should();
-
-export default describe('TEST CLIENT', function() {
-    it('TEST TO STRING', function() {
-        ['something'].should.be.a('string');
-    })
-})
--- src/server/data/config.js
+++ src/server/data/config.js
@@ -13,7 +13,7 @@ module.exports = {
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_development", // "database_test", TODO bring this back
+    "database": "mood_test", // "database_test", TODO bring this back
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
