f81294ed3456bf1cd88ed3963677a1c19d85f97f
Mihail Paley <undeadlol1@mail.ru>
2017-05-04 09:52:10 +0500

    temporary disable moodsApi tests

--- src/server/test/middlewares/moodsApi.test.js
+++ src/server/test/middlewares/moodsApi.test.js
@@ -6,37 +6,37 @@ import server from '../../server.js'
 chai.should();
 
 export default describe('/moods API CRUD', function() {
-    it('should GET moods', function() {
-        request(server)
-            .get('/api/moods')
-            .expect('Content-Type', /json/)
-            .expect('Content-Length', '15')
-            .expect(200)
-            .end(function(err, res) {
-                if (err) throw err;
-            });
-    })
+    // it('should GET moods', function() {
+        // request(server)
+        //     .get('/api/moods')
+        //     .expect('Content-Type', /json/)
+        //     .expect('Content-Length', '15')
+        //     .expect(200)
+        //     .end(function(err, res) {
+        //         if (err) throw err;
+        //     });
+    // })
 
-    it('should GET single mood', function() {
+    // it('should GET single mood', function() {
 
-    })
+    // })
 
-    it('should POST mood', function(done) {
-        request(server)
-            .post('/api/moods')
-            .expect('Content-Type', /json/)
-            .expect('Content-Length', '15')
-            .expect(200, done)
+    // it('should POST mood', function(done) {
+        // request(server)
+            // .post('/api/moods')
+            // .expect('Content-Type', /json/)
+            // .expect('Content-Length', '15')
+            // .expect(200, done)
             // .end(function(err, res) {
             //     if (err) throw err;
             // });
-    })
+    // })
 
-    it('should fail to POST if not authorized', function() { // TODO move this to previous function?
+    // it('should fail to POST if not authorized', function() { // TODO move this to previous function?
 
-    })
+    // })
 
-    it('should GET /search moods', function() {
+    // it('should GET /search moods', function() {
 
-    })
+    // })
 })
