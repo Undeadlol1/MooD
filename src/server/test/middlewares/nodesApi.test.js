b2d7e30b234621498b96efa1f2d1abbe32fd7cb5
Mihail Paley <undeadlol1@mail.ru>
2017-05-09 09:42:29 +0500

    (nodesApi.test) fix node fetching test for unlogged user

--- src/server/test/middlewares/nodesApi.test.js
+++ src/server/test/middlewares/nodesApi.test.js
@@ -1,9 +1,10 @@
 import 'babel-polyfill'
-import chai from 'chai'
+import chai, { should, expect } from 'chai'
 import request from 'supertest'
 import server from '../../server'
 import { Mood, User, Node } from '../../data/models'
 import slugify from 'slug'
+import { uniq } from 'lodash'
 chai.should();
 
 const   user = request.agent(server),
@@ -11,7 +12,14 @@ const   user = request.agent(server),
         password = "somepassword",
         moodName = "random name",
         slug = slugify(moodName),
-        url = "https://www.youtube.com/watch?v=l5-gja10qkw" 
+        url = "https://www.youtube.com/watch?v=hDH7D8_31X8",
+        urls = [
+            "https://www.youtube.com/watch?v=nBwHtgQH2EQ",
+            "https://www.youtube.com/watch?v=l5-gja10qkw",
+            "https://www.youtube.com/watch?v=M3B5U1S-I4Y",
+            "https://www.youtube.com/watch?v=P027oGJy2n4",
+            "https://www.youtube.com/watch?v=VoA9tLkrgHY",
+        ]
 
 export default describe('/nodes API', function() {
     
@@ -19,7 +27,7 @@ export default describe('/nodes API', function() {
         // TODO add logout? to test proper user login?
         // Kill supertest server in watch mode to avoid errors
         server.close()
-        // Create user and create mood
+        // Create user, mood and nodes
         user
             .post('/api/auth/signup')
             .send({ username, password })
@@ -29,7 +37,7 @@ export default describe('/nodes API', function() {
                     .post('/api/auth/login')
                     .send({ username, password })
                     .expect(302)
-                    .end(error => {
+                    .end((error, res) => {
                         if (error) return done(error)
                         user
                             .post('/api/moods')
@@ -37,6 +45,15 @@ export default describe('/nodes API', function() {
                             .expect(200)
                             .end(error => {
                                 if (error) return done(error)
+                                urls.map(url=> {
+                                    user
+                                        .post('/api/nodes')
+                                        .send({
+                                            url, moodSlug: res.body.moodslug
+                                        })
+                                        .expect(200)
+                                })
+                                if (error) return done(error)                                
                                 done()
                             })
                     })
@@ -47,7 +64,7 @@ export default describe('/nodes API', function() {
     after(function() {
         User.destroy({where: { username }})
         Mood.destroy({where: { name: moodName }})
-        Node.destroy({where: { url }})     
+        Node.destroy({where: { url }})
     })
 
     it('POST node', function(done) {
@@ -60,10 +77,10 @@ export default describe('/nodes API', function() {
                 if (err) return done(err);
                 res.body.url.should.be.equal(url)
                 done()
-        })
+            })
     })
 
-    it('GET node', function(done) {
+    it('GET single node', function(done) {
         user
             .get('/api/nodes/' + slug )
             .expect('Content-Type', /json/)
@@ -74,5 +91,44 @@ export default describe('/nodes API', function() {
                 done()
             });
     })
+
+    it('nodes cycle properly for unlogged user', async function() {
+        
+        function getNextNode(moodSlug, previousNodeId = "") {
+            return user
+                .get(`/api/nodes/${moodSlug}/${previousNodeId}`)
+                .expect(200)
+                .then(res => res.body)
+        }
+
+        try {
+            // logout user
+            await user.get('/api/auth/logout').expect(200)
+
+            const nodeIds = []
+            const mood = await Mood.findOne()
+            const initialNode = await getNextNode(mood.slug)
+            
+            // make 10 subsequent node requests
+            let nextNodeId
+            for(var x = 0; x < 10; x++) {
+                const nextNode = await getNextNode(mood.slug, nextNodeId || initialNode.id)
+                nextNodeId = nextNode.id
+                nodeIds.push(nextNode.id)
+            }
+            
+            // there must be no duplicate nodes fetched
+            expect(
+                nodeIds.length != uniq(nodeIds).length,
+                'nodes in cycle are not unique'
+            ).to.be.true
+        } catch (error) {
+            throw new Error(error)
+        }
+    })
+
+    // it('nodes cycle properly for logged in user', async function(){
+    //     throw new Error('implement this')
+    // })
 
 })
