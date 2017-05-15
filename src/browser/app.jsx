740b961e22c238838a09226f518a4e254b0f4e1b
Mihail Paley <undeadlol1@mail.ru>
2017-05-15 23:41:54 +0500

    remove apollo related code

--- src/browser/app.jsx
+++ src/browser/app.jsx
@@ -16,29 +16,15 @@ import 'es6-promise/auto';
 import 'isomorphic-fetch'
 
 import store from './redux/store'
-// TODO remove this
-import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
-import ReduxToastr from 'react-redux-toastr'
+import ReduxToastr from 'react-redux-toastr' // TODO do i even use this?
 
-// ROUTES
 import routesConfig from './routes'
 
 // STYLES
-if (process.env.BROWSER) require('./styles.scss'); //import './styles.scss'
-// require('css-modules-require-hook/preset');
+if (process.env.BROWSER) require('./styles.scss');
+
 const history = browserHistory
 // const history = syncHistoryWithStore(browserHistory, store) // for react-router-redux to work
-// const client = new ApolloClient()
-const client = new ApolloClient({
-  networkInterface:   createNetworkInterface({
-              uri: 'http://127.0.0.1:3000/graphql',
-              opts: {
-                  credentials: 'same-origin',
-                  mode: 'no-cors',
-              }
-          })
-}
-);
 
 // supply userAgent for material ui prefixer in ssr
 // http://stackoverflow.com/a/38100609
@@ -47,26 +33,24 @@ darkBaseTheme.userAgent = navigator.userAgent
 class App extends Component {
   render() {
     return  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-              {/*<ApolloProvider client={client}>*/}
-                  <ReduxProvider store={store}>
-                    <div>
-                      {
-                        process.env.BROWSER
-                        ? <Router history={browserHistory} routes={routesConfig}/>
-                        : <RouterContext {...this.props} />
-                      }
-                      <ReduxToastr
-                        timeOut={4000}
-                        newestOnTop={false}
-                        preventDuplicates={true}
-                        position="top-left"
-                        transitionIn="fadeIn"
-                        transitionOut="fadeOut"
-                        progressBar />
-                    </div>
-                  </ReduxProvider>
-                {/*</ApolloProvider>*/}
-              </MuiThemeProvider>
+              <ReduxProvider store={store}>
+                <div>
+                  {
+                    process.env.BROWSER
+                    ? <Router history={browserHistory} routes={routesConfig}/>
+                    : <RouterContext {...this.props} />
+                  }
+                  <ReduxToastr
+                    timeOut={4000}
+                    newestOnTop={false}
+                    preventDuplicates={true}
+                    position="top-left"
+                    transitionIn="fadeIn"
+                    transitionOut="fadeOut"
+                    progressBar />
+                </div>
+              </ReduxProvider>
+            </MuiThemeProvider>
   }
 }
 
