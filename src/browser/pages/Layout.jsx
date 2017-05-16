fbd3814e9f92532616359ed0fae3e084d2b92374
Mihail Paley <undeadlol1@mail.ru>
2017-05-16 21:11:10 +0500

    (Layout) use "prop-types" package

--- src/browser/pages/Layout.jsx
+++ src/browser/pages/Layout.jsx
@@ -41,7 +41,7 @@ export default class Layout extends React.Component {
 	}
 
 	static contextTypes = {
-		router: React.PropTypes.object.isRequired
+		router: PropTypes.object.isRequired
 	}
 
 	// componentDidMount() {
