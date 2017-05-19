7c9d4b1faf91e72507636067b1b1abd3b53cb60e
Mihail Paley <undeadlol1@mail.ru>
2017-05-19 13:11:55 +0500

    add Translator

--- src/browser/containers/Translator.jsx
+++ src/browser/containers/Translator.jsx
@@ -9,9 +9,32 @@ addLocaleData([...enData, ...ruData]);
 
 const messages = { en, ru }
 
+if (process.env.SERVER) {
+// https://formatjs.io/guides/runtime-environments/#server
+// var areIntlLocalesSupported = require('intl-locales-supported');
+// var localesMyAppSupports = ['en', 'ru'];
+ 
+// if (global.Intl) {
+//     // Determine if the built-in `Intl` has the locale data we need. 
+//     if (!areIntlLocalesSupported(localesMyAppSupports)) {
+//         // `Intl` exists, but it doesn't have the data we need, so load the 
+//         // polyfill and replace the constructors with need with the polyfill's. 
+//         require('intl');
+//         Intl.NumberFormat   = IntlPolyfill.NumberFormat;
+//         Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
+//     }
+// } else {
+//     // No `Intl`, so use and load the polyfill. 
+//     global.Intl = require('intl');
+// }
+}
+
 export default class Translator extends Component {
     render() {
-        return  <IntlProvider locale="en" messages={messages}>
+        const language = navigator.languages
+                            ? navigator.languages[0]
+                            : (navigator.language || navigator.userLanguage)
+        return  <IntlProvider locale={language} messages={messages[language]}>
                     {this.props.children}
                 </IntlProvider>
     }
