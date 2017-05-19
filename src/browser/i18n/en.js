12192a0103721fd23fc3af448acfb4edb7168f1a
Mihail Paley <undeadlol1@mail.ru>
2017-05-19 13:15:38 +0500

    add translations

--- src/browser/i18n/en.js
+++ src/browser/i18n/en.js
@@ -1,3 +1,11 @@
 export default {
-    test: 'This is a test!',
+    login: 'login',
+    logout: 'logout',
+    profile: 'profile',
+    about: 'about',
+    search: 'search',
+    error_accured: 'Error accured!',
+    add_your_own_mood: 'Add your own mood!',
+    choose_your_language: 'Choose your language',
+    currently_zero_content_here: 'Currently zero content here',
 }
