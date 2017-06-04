75a438513082e67df585dd7ce8e7cad5721e0ad0
Mihail Paley <undeadlol1@mail.ru>
2017-06-04 19:25:09 +0500

    (App.jsx) refactor

--- src/browser/App.jsx
+++ src/browser/App.jsx
@@ -4,30 +4,30 @@ import 'isomorphic-fetch' // TODO move to server? or to webpack?
 // material-ui dependency
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin(); 
-// supply userAgent for material-ui prefixer in ssr
-// http://stackoverflow.com/a/38100609
-darkBaseTheme.userAgent = navigator.userAgent
 
 /* DEPENDENCIES */
-import { Router, Route, browserHistory, IndexRoute, RouterContext } from 'react-router';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browserHistory, RouterContext } from 'react-router';
 import { Provider as ReduxProvider } from 'react-redux';
-import rootReducer from './redux/reducers/RootReducer'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import store from './redux/store'
 import ReduxToastr from 'react-redux-toastr'
 import routesConfig from './routes'
 import Translator from './containers/Translator'
 
-// STYLES
-if (process.env.BROWSER) require('./styles.scss');
+/* STYLES */
+if (process.env.BROWSER) require('./styles.scss')
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+// supply userAgent for material-ui prefixer in ssr
+// http://stackoverflow.com/a/38100609
+darkBaseTheme.userAgent = navigator.userAgent
+const muiTheme = getMuiTheme(darkBaseTheme)
 
 class App extends Component {
   render() {
-    return  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    return  <MuiThemeProvider muiTheme={muiTheme}>
               <ReduxProvider store={store}>
                 <Translator>
                     {
