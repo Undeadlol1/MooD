db4c8446de31d1c82920f50071f4e4150bd40b3a
Mihail Paley <undeadlol1@mail.ru>
2017-06-09 03:09:07 +0500

    fix reduxDevtools

--- src/browser/redux/store.js
+++ src/browser/redux/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk' // TODO is it even used?
+import { createStore, applyMiddleware } from 'redux'
+ // TODO is it even used?
+import thunk from 'redux-thunk'
 import rootReducer from './reducers/RootReducer'
-// console.log('window', window)
-// const reduxDevtools =   window && (process.env.NODE_ENV == 'development')
-//                         ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//                         : {}
+
+const reduxDevtools =   process.env.BROWSER && process.env.NODE_ENV == 'development'
+                        ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                        : {}
                         
 const store =   createStore(
                     rootReducer,
-                    // reduxDevtools,
+                    reduxDevtools,
                     applyMiddleware(thunk) // TODO do i use this?
                 ) // thunk, promise, // TODO add dev variable to redux devtools
 
