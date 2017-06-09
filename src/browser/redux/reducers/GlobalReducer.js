751caf14ce15aa8b66afc7206ea18af341422a25
Mihail Paley <undeadlol1@mail.ru>
2017-06-09 05:18:12 +0500

    (GlobalReducer) make initial state exportable

--- src/browser/redux/reducers/GlobalReducer.js
+++ src/browser/redux/reducers/GlobalReducer.js
@@ -2,14 +2,11 @@ import { fromJS } from 'immutable'
 import { isBoolean } from 'lodash'
 // TODO this is a mess, reworking needed
 // TODO rename to UiReducer
-const initialState =	fromJS({
-							headerIsShown: true,
-							sidebarIsOpen: false,
-							controlsAreShown: false,
-							// TODO delete this
-							error: '', // never used atm
-                            loading: true, // change to null?
-                        })
+export const initialState =	fromJS({
+								headerIsShown: true,
+								sidebarIsOpen: false,
+								controlsAreShown: false,
+							})
 
 export default (state = initialState, { type, payload }) => {
 	/**
