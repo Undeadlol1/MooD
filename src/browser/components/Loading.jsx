d8ea5bdc3bdc4bd3e98f37f37353f9908b8d3043
Mihail Paley <undeadlol1@mail.ru>
2017-06-09 12:50:53 +0500

    (Loading) add ability to use as wrapper

--- src/browser/components/Loading.jsx
+++ src/browser/components/Loading.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from 'react'
 import CircularProgress from 'material-ui/CircularProgress';
 
-export default props => <CircularProgress className="Loading" />
+/**
+ * show loading indicator
+ * could be used as a wrapper via 'condition' prop
+ */
+export default ({condition = true, children, className}) => {
+    if (!condition) return children
+    else return <CircularProgress className={"Loading " + className} />
+}
