7bcad6d878b012835a9a77a64f634c05c2154e0c
Mihail Paley <undeadlol1@mail.ru>
2017-06-12 06:15:07 +0500

    (Loading) add conditional behaviour

--- src/browser/components/Loading.jsx
+++ src/browser/components/Loading.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import CircularProgress from 'material-ui/CircularProgress';
+import classNames from 'classnames'
+import { isEmpty } from 'lodash'
 
 /**
  * show loading indicator
  * could be used as a wrapper via 'condition' prop
  */
-export default ({condition = true, children, className}) => {
-    if (!condition) return children
-    else return <CircularProgress className={"Loading " + className} />
+export default function Loading ({condition, children, className}) {
+    if (condition == false && !isEmpty(children)) return children
+    return <CircularProgress className={classNames("Loading", className)} />
 }
