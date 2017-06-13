08baf652704300dbb2cb0bdb0780012b6c86e678
Mihail Paley <undeadlol1@mail.ru>
2017-06-13 06:12:28 +0500

    (UserReducer) fix multiple merging problems

--- src/browser/redux/reducers/UserReducer.js
+++ src/browser/redux/reducers/UserReducer.js
@@ -29,13 +29,13 @@ export default (state = initialState, { type, payload }) => {
 		case 'FETCHING_IN_PROGRESS':
 			return state.set('loading', true)
 		case 'RECIEVE_CURRENT_USER':
-			return state.merge({
+			return state.mergeDeep({
 				...payload,
 				loading: false,
 				loginIsOpen: isEmpty(payload) && state.get('loading') && state.get('loginIsOpen')
 			})
 		case 'RECIEVE_FETCHED_USER':
-			return state.set({
+			return state.mergeDeep({
 				loading: false,
 				fetchedUser: payload
 			})
@@ -44,7 +44,7 @@ export default (state = initialState, { type, payload }) => {
 				fetchedUser: emptyUserObject
 			})
 		case 'REMOVE_CURRENT_USER':
-			return state.mergeWith(emptyUserObject)
+			return state.merge(emptyUserObject)
 		case 'TOGGLE_LOGIN_DIALOG':
 			return state.set('loginIsOpen', payload)
 		default:
