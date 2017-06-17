01d94c1b617d6862127c33e78ac5e05d82ff111f
Mihail Paley <undeadlol1@mail.ru>
2017-06-17 18:26:42 +0500

    (SSR) prerendered markup comes with loading indicator now

--- src/browser/components/Loading.jsx
+++ src/browser/components/Loading.jsx
@@ -7,8 +7,14 @@ import isEmpty from 'lodash/isEmpty'
  * show loading indicator
  * could be used as a wrapper via 'condition' prop
  */
-export default function Loading ({condition, children, className}) {
+export default function Loading ({condition, children, style, className}) {
+    const styles = style || {
+        top: '50%',
+        left: '50%',
+        position: 'absolute', //!important;
+        transform: 'translate(-50%, -50%)',
+    }
     const cx = classNames('Loading', className)
-    if (condition == false && !isEmpty(children)) return <div className={className}>{children}</div>
-    return <CircularProgress className={cx} />
+    if (!condition && !isEmpty(children)) return <div className={className}>{children}</div>
+    return <div style={styles}><CircularProgress style={styles} className={cx} /></div>
 }
