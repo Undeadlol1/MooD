f0af9f214caaf73b669d6df903fcd1d0cd900dc8
Mihail Paley <undeadlol1@mail.ru>
2017-06-20 15:56:04 +0500

    remove username form 'users' table

--- src/server/data/models/user.js
+++ src/server/data/models/user.js
@@ -3,11 +3,6 @@ var bcrypt   = require('bcrypt-nodejs');
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
     password: DataTypes.STRING,
     image: DataTypes.STRING,
     facebook_id: DataTypes.STRING,
