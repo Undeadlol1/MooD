71dfe5f6f826efb2d50bc6259f47e744b06828b7
Mihail Paley <undeadlol1@mail.ru>
2017-06-22 19:20:52 +0500

    add localAuth

--- src/server/middlewares/authApi/localAuth.js
+++ src/server/middlewares/authApi/localAuth.js
@@ -1,78 +1,92 @@
 import { Strategy as LocalStrategy } from "passport-local"
-import { User, Profile } from 'server/data/models'
+import { User, Local, Profile } from 'server/data/models'
+import { Router } from "express"
 import passport from "passport"
-import express from "express"
-import selectn from 'selectn'
 
 const { URL } = process.env
+const router = Router()
 
-/* LOCAL AUTH */
-passport.use('local-login', new LocalStrategy(
-  function(username, password, done) {
-    if(!username || !password) throw new Error('forgot credentials') // TODO this
-    User.findOne({ where: { username }})
-        .then(user => {
-            if (!user) {
-              return done(null, false, { message: 'Username not found' });
-            }
-            if (!user.validPassword(password)) {
-              return done(null, false, { message: 'Incorrect password' });
-            }
-            return done(null, user);
-        })
-        .catch(error => done(error))
-  }
-));
-
-passport.use('local-signup', new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password',
-    passReqToCallback : true },
-    function(req, username, password, done) {
-      process.nextTick(function() {
-        if(!username || !password) throw new Error() // TODO this
-          // find a user whose email is the same as the forms email
-          // we are checking to see if the user trying to login already exists
-          User.findOne({where: { username }})
-              .then(user => {
-                // console.log('user', user)
-                if (user) {
-                    throw new Error()
-                    // return done(null, false);//, req.flash('signupMessage', 'That username is already taken.')
-                } else {
-                  User.create({
-                    username,
-                    image: '/userpic.png',
-                    password: User.generateHash(password),
-                    Profile: {}
-                  }, {include: [Profile]})
-                  .then(newUser => done(null, newUser))
-                  .catch(error => {
-                    console.error(error)
-                    done(error)
-                  })
-                }
-              })
-              .catch(error => {
-                console.log(error)
-                done(error)
-              })
-    });
-}));
-
-// routes
-const router = express.Router(); // TODO refactor without "const"?
 router
-  .post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/xxx', // redirect back to the signup page if there is an error // TODO add failure redirect
-    failureFlash : true // allow flash messages
-  }))
+  .post('/signup',   async function(req, res) {
+    const { username, email, password } = req.body
+    // TODO test for params
+    if(!username || !email || !password) res.status(400).end('Invalid query')
+    // find a user whose email is the same as the forms email
+    // we are checking to see if the user trying to login already exists
 
-  .post('/login', passport.authenticate('local-login', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/xxxx', // redirect back to the signup page if there is an error // TODO add failure redirect
-    failureFlash : true // allow flash messages
-  }))
+    // TODO add validation tests
+    // TODO move everything into user controller!!
+    try {
+        const existingUser = await Local.findOne({
+                                    where: {
+                                      $or: [{email}, {username}]
+                                  }})
+        if (existingUser) res.status(401).end('user already exists')
+        else {
+          // TODO try include again
+          const user = await User.create({}, {raw: true})
+          const local = await Local.create({
+            email,
+            username,
+            UserId: user.id,
+            password: Local.generateHash(password) // TODO add tests
+          }, {raw: true})
+          const newUser = await User.findOne({
+            where: {id: user.id},
+            include: [Local, Profile],
+            raw: true,
+            nest: true,
+          })
+          req.login(newUser, error => {
+            if (error) throw new Error(error)
+            else return res.json(newUser)
+          })
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).end(error)
+    }
+  })
+
+  .post('/login', async function(req, res) {
+      try {
+        const {username, password} = req.body
+        if(!username || !password) return res.status(400).end('Invalid query')
+        // TODO this
+        const email = username
+        const user =  await User.findOne({
+                        where: {},
+                        include: [Profile, {
+                          model: Local,
+                          where: { $or: [{email}, {username}] },
+                        }],
+                      })
+        if (!user) {
+          return res.status(401).end('User not exists')
+        }
+        if (!user.Local.validPassword(password)) {
+          const newLocal = await Local.findById(user.Local.id)
+          return res.status(401).end('Incorrect password')
+        }
+        req.login(user, error => {
+          if (error) throw new Error(error)
+          else return res.json(user)
+        })
+      } catch (error) {
+          console.error(error)
+          res.status(500).end(error)
+      }
+  })
+  // TODO tests
+  .get('/validate/:username', async (req, res) => {
+    const {username} = req.params
+    const user = await Local.findOne({
+      where: {
+        $or: [{username}, {email: username}]
+      },
+      raw: true,
+    })
+    res.json(user || {})
+  })
 
 export default router
--- src/server/test/middlewares/usersApi.test.js
+++ src/server/test/middlewares/usersApi.test.js
@@ -1,7 +1,7 @@
 import chai, { assert } from 'chai'
 import request from 'supertest'
 import server from '../../server.js'
-import { User, Profile } from '../../data/models'
+import { User, Local, Profile } from '../../data/models'
 import select from 'selectn'
 import { loginUser } from './authApi.test'
 import users from '../../data/fixtures/users'
@@ -23,13 +23,14 @@ export default describe('/users API', function() {
         try {
             const user = await User.findOne({order: 'rand()'})
             await request(server)
-                .get(`/api/users/user/${user.username}`)
+                .get(`/api/users/user/${user.id}`)
                 .expect(200)
                 .expect('Content-Type', /json/)
-                .then(function(res) {
-                    assert(select('body.id', res), 'must have an id')
-                    assert(select('body.Profile.id', res), 'must have a profile')
-                    assert(res.body.username == user.username, 'must have appropriate name')
+                .then(function({body}) {
+                    assert(body.id, 'must have an id')
+                    assert(body.Profile, 'must have Profile')
+                    // TODO
+                    // assert(!body.Local, 'must not have Local')
                 })
         }
         catch (error) {
@@ -40,10 +41,11 @@ export default describe('/users API', function() {
 
     it('update user profile', async function() {
         try {
-            const user = users[0]
-            const agent = await loginUser(user.username, user.password)
+            const {username, password} = users[0]
+            const localAuth = await Local.findOne({where: {username}})
+            const agent = await loginUser(username, password)
             await agent
-                .put(`/api/users/user/${user.username}`)
+                .put(`/api/users/user/${localAuth.UserId}`)
                 .send({language: 'fr'})
                 .expect(200)
                 .expect('Content-Type', /json/)
