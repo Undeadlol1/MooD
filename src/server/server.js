db1a41615ca053f55d8d132ff598def85696ff2d
Mihail Paley <undeadlol1@mail.ru>
2017-06-25 21:55:41 +0500

    (server) add delay to rateLimiter

--- src/server/server.js
+++ src/server/server.js
@@ -30,7 +30,7 @@ const port = process.env.PORT || 3000,
 const limiter = new RateLimiter({
   windowMs: 15*60*1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
-  delayMs: 0 // disable delaying - full speed until the max limit is reached
+  delayMs: 200 // disable delaying - full speed until the max limit is reached
 });
 
 // development only middlewares
