59d20b80765473174a0f1cdd9abf9513acda09d1
Mihail Paley <undeadlol1@mail.ru>
2017-06-25 17:58:40 +0500

    (Loading) add proptypes and 'color' prop

--- src/browser/components/Loading.jsx
+++ src/browser/components/Loading.jsx
@@ -1,13 +1,14 @@
+import CircularProgress from 'material-ui/CircularProgress'
 import React, { Component } from 'react'
-import CircularProgress from 'material-ui/CircularProgress';
-import classNames from 'classnames'
 import isEmpty from 'lodash/isEmpty'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
 
 /**
  * show loading indicator
  * could be used as a wrapper via 'condition' prop
  */
-export default function Loading ({condition, children, style, className}) {
+function Loading ({condition, children, color, style, className}) {
     const styles = style || {
         top: '50%',
         left: '50%',
@@ -16,5 +17,12 @@ export default function Loading ({condition, children, style, className}) {
     }
     const cx = classNames('Loading', className)
     if (!condition && !isEmpty(children)) return <div className={className}>{children}</div>
-    return <div style={styles} className={className} ><CircularProgress className="loading" style={styles} /></div>
+    return <div style={styles} className={className} ><CircularProgress color={color} className="loading" style={styles} /></div>
 }
+
+Loading.propTypes = {
+    color: PropTypes.string,
+    condition: PropTypes.bool,
+}
+
+export default Loading
