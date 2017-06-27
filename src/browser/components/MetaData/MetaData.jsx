6ce9e6073d45af71dc70363327d1410bfab78f6a
Mihail Paley <undeadlol1@mail.ru>
2017-06-27 16:15:46 +0500

    (MetaData) fix image and description props

--- src/browser/components/MetaData/MetaData.jsx
+++ src/browser/components/MetaData/MetaData.jsx
@@ -3,40 +3,40 @@ import { Helmet } from 'react-helmet'
 import PropTypes from 'prop-types'
 
 export class MetaData extends Component {
+	// copy+paste from https://megatags.co/#generate-tags
 	render() {
-		const {props} = this
-		// copy+paste from https://megatags.co/#generate-tags
+		const { appUrl, title, description, image, } = this.props
 	    return  <Helmet>
 					{/* COMMON TAGS */}
 					<meta charset="utf-8" />
-					<title>{props.title}</title>
+					<title>{title}</title>
 					{/* Search Engine */}
 					{/* 150 characters for SEO, 200 characters for Twitter & Facebook */}
-					{/*<meta name="description" content="MooD - is the place where you share your mood with others" />*/}
-					<meta name="image" content="http://moodx.ru/android-chrome-192x192.png" />
+					<meta name="description" content={description} />
+					<meta name="image" content={image} />
 					{/* Schema.org for Google */}
-					<meta itemprop="name" content={props.title} />
-					{/*<meta itemprop="description" content="MooD - is the place where you share your mood with others" />*/}
-					<meta itemprop="image" content="http://moodx.ru/android-chrome-192x192.png" />
+					<meta itemprop="name" content={title} />
+					<meta itemprop="description" content={description} />
+					<meta itemprop="image" content={image} />
 					{/* Twitter */}
 					<meta name="twitter:card" content="summary" />
-					<meta name="twitter:title" content={props.title} />
-					{/*<meta name="twitter:description" content="MooD - is the place where you share your mood with others" />*/}
+					<meta name="twitter:title" content={title} />
+					<meta name="twitter:description" content={description} />
 					{/*<meta name="twitter:site" content="@publishehadle_sdd" />*/}
 					{/*<meta name="twitter:creator" content="@auth_handledee" />*/}
 					{/* Maximum dimension: 1024px x 512px; minimum dimension: 440px x 220px */}
-					{/*<meta name="twitter:image:src" content="http://moodx.ru/android-chrome-192x192.png" />*/}
+					<meta name="twitter:image:src" content={image} />
 					{/* HTTPS URL to an iFrame player */}
 					{/* TODO add 'videoId' property */}
 					{/*<meta name="twitter:player" content="https://www.youtube.com/watch?v=EDJsVbSZb-Q" />*/}
 					{/* Open Graph general (Facebook, Pinterest & Google+) */}
-					<meta name="og:title" content={props.title} />
-					{/*<meta name="og:description" content="MooD - is the place where you share your mood with others" />*/}
+					<meta name="og:title" content={title} />
+					<meta name="og:description" content={description} />
 					{/* Recommended dimension: 1200px x 630px; minimum dimension: 600px x 315px */}
-					{/*<meta name="og:image" content="http://moodx.ru/android-chrome-192x192.png" />*/}
-					<meta name="og:url" content={props.appUrl} />
+					<meta name="og:image" content={image} />
+					<meta name="og:url" content={appUrl} />
 					{/* TODO: sietename or site title? */}
-					<meta name="og:site_name" content={props.title} />
+					<meta name="og:site_name" content={title} />
 					{/* TODO dynamic locales */}
 					<meta name="og:locale" content="ru_RU" />
 					{/*<meta name="og:video" content="https://www.youtube.com/watch?v=EDJsVbSZb-Q" />*/}
@@ -49,13 +49,17 @@ export class MetaData extends Component {
 }
 
 MetaData.defaultProps = {
-	title: process.env.APP_NAME,
 	appUrl: process.env.URL,
+	title: process.env.APP_NAME,
+	description: 'MooD - музыка твоего настроения',
+	image: "http://moodx.ru/android-chrome-192x192.png",
 }
 
 MetaData.PropTypes = {
 	title: PropTypes.string.isRequired,
+	image: PropTypes.string.isRequired,
 	appUrl: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
 }
 
 export default MetaData
