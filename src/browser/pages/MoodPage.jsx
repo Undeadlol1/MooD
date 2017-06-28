c3572e0bf7052a3465628cabc4fe11a19d4d9b02
Mihail Paley <undeadlol1@mail.ru>
2017-06-29 01:43:56 +0500

    (MoodPage) fix metadata props

--- src/browser/pages/MoodPage.jsx
+++ src/browser/pages/MoodPage.jsx
@@ -35,8 +35,8 @@ export class MoodPage extends Component {
 		return 	<PageWrapper
 					loading={isLoading}
 					className="MoodPage"
-					title={t('current_mood') + props.moodName}
-					image={props.videoId && `http://img.youtube.com/vi/${props.videoId}/hqdefault.jpg`}
+					title={!isLoading && t('current_mood') + props.moodName}
+					image={isLoading && props.videoId && `http://img.youtube.com/vi/${props.videoId}/hqdefault.jpg`}
 				>
 					{/* TODO remove h1 (use css instead) */}
 					{contentNotFound && <h1 className="MoodPage__header">{t("currently_zero_content_here")}</h1>}
