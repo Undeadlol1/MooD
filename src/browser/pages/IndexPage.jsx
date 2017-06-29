cc12626c2bebde2488138af2c3cce8e85bdf0ea6
Mihail Paley <undeadlol1@mail.ru>
2017-06-29 21:53:40 +0500

    (IndexPage) add async data

--- src/browser/pages/IndexPage.jsx
+++ src/browser/pages/IndexPage.jsx
@@ -1,23 +1,27 @@
 // dependencies
-import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import React, { Component } from 'react'
+import { asyncConnect } from 'redux-connect'
 import { Grid, Row } from 'react-styled-flexboxgrid'
 // project files
 import Loading from 'browser/components/Loading'
 import MoodsFind from 'browser/components/MoodsFind'
 import MoodsList from 'browser/components/MoodsList'
 import MoodsInsert from 'browser/components/MoodsInsert'
-import YoutubeSearch from 'browser/components/YoutubeSearch'
 import PageWrapper from 'browser/components/PageWrapper'
+import YoutubeSearch from 'browser/components/YoutubeSearch'
 import { fetchMoods } from 'browser/redux/actions/MoodActions'
+import { parseJSON } from 'browser/redux/actions/actionHelpers'
+import { fromJS } from 'immutable'
+
+const {API_URL} = process.env
+const moodsUrl = API_URL + 'moods/'
 
 export class IndexPage extends Component {
-
-	componentWillMount() { this.props.fetchMoods() }
-
     render() {
-		const {loading, moods, currentPage, totalPages, location} = this.props
+		const { props } = this
+		const {prefetchedMoods, loading} = props
 		return 	<PageWrapper
 					loading={loading}
 					className='IndexPage'
@@ -27,9 +31,9 @@ export class IndexPage extends Component {
 						{/* TODO what to do with this loading? */}
 						<Loading condition={loading}>
 							<MoodsList
-								moods={moods}
-								totalPages={totalPages}
-								currentPage={currentPage} />
+								moods={prefetchedMoods.moods || props.moods}
+								totalPages={prefetchedMoods.totalPages || props.totalPages}
+								currentPage={prefetchedMoods.currentPage || props.currentPage} />
 						</Loading>
 					</Grid>
 				</PageWrapper>
@@ -46,7 +50,24 @@ IndexPage.propTypes = {
 }
 
 export default
-connect(
+asyncConnect([
+	{
+		key: 'prefetchedMoods',
+		promise: ({ params, helpers }) => {
+			return fetch(moodsUrl)
+			.then(parseJSON)
+			.then(response => 	{
+				response.moods = fromJS(response.moods)
+				return response
+			})
+			.catch(error => {
+				console.error(error)
+				throw new Error(error)
+			})
+		}
+	}
+])
+(connect(
 	({ mood }) => ({
 		moods: mood.get('moods'),
 		loading: mood.get('loading'),
@@ -56,4 +77,4 @@ connect(
 	dispatch => ({
 		fetchMoods() {dispatch(fetchMoods())}
 	})
-)(IndexPage)
+)(IndexPage))
