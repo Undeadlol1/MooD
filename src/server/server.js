088ad35af0ac185c7ec0f78724a9ffd7876636e6
Mihail Paley <undeadlol1@mail.ru>
2017-07-01 19:54:23 +0500

    (server) add simple redis caching

--- src/server/server.js
+++ src/server/server.js
@@ -22,6 +22,7 @@ import helmet from 'helmet'
 import createLocaleMiddleware from 'express-locale';
 import RateLimiter from 'express-rate-limit'
 const RedisStore = require('connect-redis')(session)
+var cache = require('express-redis-cache')();
 
 // TODO add server side bundle minification to improve perfomance
 
@@ -106,60 +107,73 @@ import { Provider } from 'react-redux'
 import store from 'browser/redux/store'
 import 'isomorphic-fetch' // fetch polyfill
 
+// TODO move this to middleware
 // all routes are processed client side via react-router
-app.get('/*', function(req, res) {
-  match(
-      {routes, location: req.url},
-      (error, redirectLocation, renderProps) => {
-        if (error) res.status(500).send(error.message)
+app.get('/*',
 
-        else if (redirectLocation) {
-          const location =  redirectLocation.pathname
-                            + redirectLocation.search
-          res.redirect(302, location)
-        }
-        // render website content
-        else if (renderProps) {
-          const sheet = new ServerStyleSheet()
-          /*
-            sometimes request language and browser language are not the same
-            so we use browsers language (storred in cookie) as primary preference
-          */
-          const cookieLocale = req.cookies.locale
-          const requestLocale = req.locale.language
-          const language = cookieLocale || requestLocale
-          global.navigator = global.navigator || {language};
-          /*
-            supply userAgent for material ui prefixer in ssr
-            http://stackoverflow.com/a/38100609
-          */
-          global.navigator.userAgent = req.headers['user-agent'] || 'all';
-          // require App after userAgent is set
-          const App = require('browser/App').default
-          // render App to string
-          const markup = renderToString(
-            <StyleSheetManager sheet={sheet.instance}>
-              <App {...renderProps}/>
-            </StyleSheetManager>
-          )
-          // extract css from string
-          const css = sheet.getStyleTags()
-          // extract metaData for <header>
-          let headerTags = []
-          const metaData = Helmet.renderStatic()
-          for (var prop in metaData) {
-            const tag = metaData[prop].toString()
-            tag && headerTags.push(tag)
+  // middleware to define cache prefix
+  function (req, res, next) {
+    // set cache name
+    res.express_redis_cache_name = 'url-' + req.url
+    next();
+  },
+
+  // cache middleware
+  cache.route(),
+  // markup renderer
+  function(req, res) {
+    match(
+        {routes, location: req.url},
+        (error, redirectLocation, renderProps) => {
+          if (error) res.status(500).send(error.message)
+
+          else if (redirectLocation) {
+            const location =  redirectLocation.pathname
+                              + redirectLocation.search
+            res.redirect(302, location)
           }
-          // get prefetched data from redux
-          // TODO make sure to reset state afterwards
-          const initialData = JSON.stringify(store.getState())//.replace(/</g, '\\u003c')
-          // send data to handlebars template
-          res.render('index', { markup, css, headerTags, initialData })
-        }
+          // render website content
+          else if (renderProps) {
+            const sheet = new ServerStyleSheet()
+            /*
+              sometimes request language and browser language are not the same
+              so we use browsers language (storred in cookie) as primary preference
+            */
+            const cookieLocale = req.cookies.locale
+            const requestLocale = req.locale.language
+            const language = cookieLocale || requestLocale
+            global.navigator = global.navigator || {language};
+            /*
+              supply userAgent for material ui prefixer in ssr
+              http://stackoverflow.com/a/38100609
+            */
+            global.navigator.userAgent = req.headers['user-agent'] || 'all';
+            // require App after userAgent is set
+            const App = require('browser/App').default
+            // render App to string
+            const markup = renderToString(
+              <StyleSheetManager sheet={sheet.instance}>
+                <App {...renderProps}/>
+              </StyleSheetManager>
+            )
+            // extract css from string
+            const css = sheet.getStyleTags()
+            // extract metaData for <header>
+            let headerTags = []
+            const metaData = Helmet.renderStatic()
+            for (var prop in metaData) {
+              const tag = metaData[prop].toString()
+              tag && headerTags.push(tag)
+            }
+            // get prefetched data from redux
+            // TODO make sure to reset state afterwards
+            const initialData = JSON.stringify(store.getState())//.replace(/</g, '\\u003c')
+            // send data to handlebars template
+            res.render('index', { markup, css, headerTags, initialData })
+          }
 
-        else res.status(404).send('Not found')
-  } );
+          else res.status(404).send('Not found')
+    } );
 })
 
 // export app to use in test suits
