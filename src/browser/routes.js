9df32e577051a073cf80387829cea328c18bb96c
Mihail Paley <undeadlol1@mail.ru>
2017-07-01 07:21:39 +0500

    (routes) add data fetching

--- src/browser/routes.js
+++ src/browser/routes.js
@@ -7,15 +7,43 @@ import MoodPage from './pages/MoodPage';
 import SearchPage from './pages/SearchPage';
 import AboutPage from './pages/AboutPage';
 import NotFound from './pages/NotFound';
+import store from 'browser/redux/store'
+import { fetchMoods, fetchMood } from 'browser/redux/actions/MoodActions'
+import { fetchNode, actions as nodeActions } from 'browser/redux/actions/NodeActions'
 
 const routesConfig = {
   path: '/',
   component: Layout,
-  indexRoute: { component: IndexPage },
+  indexRoute: {
+    component: IndexPage,
+    // fetch data
+    onEnter({params}, replace, callback) {
+      const fetchedMoods = store.getState().mood.get('moods')
+      // check if fetching is needed
+      if (fetchedMoods.size) return callback()
+      else {
+        store
+        .dispatch(fetchMoods())
+        .then(() => callback())
+      }
+    }
+  },
   childRoutes: [
     { path: 'login', component: LoginPage },
     { path: 'users/(:username)', component: UserPage },
-    { path: 'mood/(:moodSlug)', component: MoodPage },
+    {
+      component: MoodPage,
+      path: 'mood/(:moodSlug)',
+      // fetch data
+      onEnter({params}, replace, callback) {
+        Promise
+        .all([
+          store.dispatch(fetchMood(params.moodSlug)),
+          store.dispatch(fetchNode(params.moodSlug)),
+        ])
+        .then(() => callback())
+      }
+    },
     { path: 'search', component: SearchPage },
     { path: 'about', component: AboutPage },
     { path: '*', component: NotFound },
