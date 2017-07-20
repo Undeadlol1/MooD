dfa8610757742deefd9ee8d686d45137e6f43fca
Mihail Paley <undeadlol1@mail.ru>
2017-07-20 23:22:04 +0500

    (externalsApi) restrict youtube search to videos only

--- src/server/middlewares/externalsApi.js
+++ src/server/middlewares/externalsApi.js
@@ -10,14 +10,18 @@ import YouTube from 'youtube-node'
 /*
   ⚠️ note about YouTube API ⚠️
   currently youtube key works only on specific IP's
-  so, when multi server infrastructure is created ither
+  so, when multi server infrastructure is created either
   add server IP addresses in YouTube dashboard,
   or bound key to website URL and move YT search to client
 */
-const youTube = new YouTube();
+const youTube = new YouTube(),
+      maxResults = 4,
+      options = {
+                  maxResults,
+                  type: 'video',
+                  videoEmbeddable: true,
+                }
 youTube.setKey(process.env.YOUTUBE_KEY);
-youTube.addParam('type', 'video')
-youTube.addParam('videoEmbeddable', true)
 
 // routes
 export default Router()
@@ -25,7 +29,7 @@ export default Router()
     try {
       const selector = req.query.query // TODO rework this ✏️
       if (!selector || isEmpty(selector)) return res.boom.badRequest('invalid query')
-      youTube.search(selector, 4,
+      youTube.search(selector, maxResults, options,
         (error, result) => {
           if (error) throw error
           else res.json(result.items)
