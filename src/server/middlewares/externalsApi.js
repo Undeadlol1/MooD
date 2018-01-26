f6a8ae7794df8fc95bc9b0d416e8170b383af2c8
Mihail Paley <undeadlol1@mail.ru>
2018-01-27 02:29:23 +0500

    bring back youtube-node

--- src/server/middlewares/externalsApi.js
+++ src/server/middlewares/externalsApi.js
@@ -4,7 +4,7 @@ import { parse as parseQuery } from 'query-string'
 import { parseUrl } from 'shared/parsers'
 import extend from 'lodash/assignIn'
 import isEmpty from 'lodash/isEmpty'
-// import YouTube from 'youtube-node'
+import YouTube from 'youtube-node'
 import sequelize from "sequelize"
 import { Router } from "express"
 
@@ -22,27 +22,26 @@ import { Router } from "express"
   add server IP addresses in YouTube dashboard,
   or bound key to website URL and move YT search to client
 */
-// const youTube = new YouTube(),
-//       maxResults = 4,
-//       options = {
-//                   maxResults,
-//                   type: 'video',
-//                   videoEmbeddable: true,
-//                 }
-// youTube.setKey(process.env.YOUTUBE_KEY);
+const youTube = new YouTube(),
+      maxResults = 4,
+      options = {
+                  maxResults,
+                  type: 'video',
+                  videoEmbeddable: true,
+                }
+youTube.setKey(process.env.YOUTUBE_KEY);
 
-// // routes
+// routes
 export default Router()
   .get('/search', async function(req, res) {
     try {
-      res.end()
-      // const selector = req.query.query // TODO rework this ✏️
-      // if (!selector || isEmpty(selector)) return res.boom.badRequest('invalid query')
-      // youTube.search(selector, maxResults, options,
-      //   (error, result) => {
-      //     if (error) throw error
-      //     else res.json(result.items)
-      // });
+      const selector = req.query.query // TODO rework this ✏️
+      if (!selector || isEmpty(selector)) return res.boom.badRequest('invalid query')
+      youTube.search(selector, maxResults, options,
+        (error, result) => {
+          if (error) throw error
+          else res.json(result.items)
+      });
     } catch (error) {
       console.error(error);
       res.boom.internal(error)
