6fcd86d8df11c68551df3741ce6f9f91f2e22f13
Mihail Paley <undeadlol1@mail.ru>
2018-02-18 18:28:57 +0500

    (decisionsApi) enable POST test

--- src/server/test/middlewares/decisionsApi.test.js
+++ src/server/test/middlewares/decisionsApi.test.js
@@ -8,8 +8,8 @@ import { loginUser } from './authApi.test'
 import chai, { should, expect } from 'chai'
 import users from 'server/data/fixtures/users'
 import { Mood, User, Local, Node, Decision } from 'server/data/models'
-chai.use(require('chai-datetime'));
-chai.should();
+chai.use(require('chai-datetime'))
+chai.should()
 
 const   agent = request.agent(server),
         apiUrl = '/api/decisions/',
@@ -17,49 +17,33 @@ const   agent = request.agent(server),
         vote = true
 
 export default describe('/decisions API', function() {
+    // Kill supertest server in watch mode to avoid errors
+    before(() => server.close())
 
-    before(function() {
-        // TODO add logout? to test proper user login?
-        // Kill supertest server in watch mode to avoid errors
-        server.close()
+    describe('POST', async function() {
+
+        it('creates decision', async function() {
+            const node = await Node.findOne({order: 'rand()'})
+            const loggedIn = await loginUser(user.username, user.password)
+            await loggedIn
+                .post(apiUrl)
+                .send({vote, NodeId: node.id})
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .then(async ({body}) => {
+                    expect(body.vote).eq(true)
+                    // TODO check this
+                    Number(node.rating) > 0
+                    ? expect(body.NodeRating).above(node.rating)
+                    : expect(body.NodeRating).below(node.rating)
+                    // make sure node.rating is updated
+                    const updatedNode = await Node.findById(node.id)
+                    expect(updatedNode.rating == body.NodeRating)
+                })
+        })
+
+        it('fails if not logged in', async () => await agent.post(apiUrl).expect(401))
     })
-
-    // clean up
-    after(function() {
-        server.close()
-    })
-
-
-    // describe('POST', async function() {
-
-    //     it('creates decision', async function() {
-    //         const node = await Node.findOne({order: 'rand()'})
-    //         const loggedIn = await loginUser(user.username, user.password)
-    //         await loggedIn
-    //                 .post(apiUrl)
-    //                 .send({
-    //                     vote,
-    //                     NodeId: node.id,
-    //                 })
-    //                 .expect(200)
-    //                 .expect('Content-Type', /json/)
-    //                 .then(async ({body}) => {
-    //                     expect(body.vote).eq(true)
-    //                     // TODO check this
-    //                     Number(node.rating) > 0
-    //                     ? expect(body.NodeRating).above(node.rating)
-    //                     : expect(body.NodeRating).below(node.rating)
-    //                     // make sure node.rating is updated
-    //                     const updatedNode = await Node.findById(node.id)
-    //                     expect(updatedNode.rating == body.NodeRating)
-    //                 })
-    //     })
-
-
-    //     it('fails if not logged in', async function() {
-    //         await agent.post(apiUrl).expect(401)
-    //     })
-    // })
 
     // describe('PUT', async function() {
 
