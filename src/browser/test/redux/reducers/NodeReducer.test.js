90fa984253e7884dfcaf1495b9efece83cd6bff4
Mihail Paley <undeadlol1@mail.ru>
2018-02-19 21:25:45 +0500

    (NodeReducer) fix UPDATE_NODE action

--- src/browser/test/redux/reducers/NodeReducer.test.js
+++ src/browser/test/redux/reducers/NodeReducer.test.js
@@ -1,12 +1,13 @@
 import chai, { expect } from 'chai'
-import { Map, List } from 'immutable'
+import { Map, List, fromJS } from 'immutable'
 import chaiImmutable from 'chai-immutable'
 import { actions } from 'browser/redux/actions/NodeActions'
 import reducer, { initialState } from 'browser/redux/reducers/NodeReducer'
-chai.should();
-chai.use(chaiImmutable);
+chai.should()
+chai.use(chaiImmutable)
+chai.use(require('chai-properties'))
 
-describe('user reducer', async () => {
+describe('node reducer', () => {
 
   const node =  {
     id: 1,
@@ -20,48 +21,61 @@ describe('user reducer', async () => {
     Decision: {},
   }
   const nodes = [
-    {id: 1},
+    node,
     {id: 2},
     {id: 3},
   ]
 
   it('should have initial state', () => {
-    expect(reducer(undefined, {})).to.equal(initialState);
-  });
+    expect(reducer(undefined, {})).to.equal(initialState)
+  })
 
-  it('should handle RECIEVE_NODE action on initial state', async () => {
+  it('should handle RECIEVE_NODE action on initial state', () => {
     const action = actions.recieveNode(node)
     const newState = reducer(undefined, action)
     expect(newState).to.have.property('id', node.id)
     expect(newState).to.have.property('contentId', node.contentId)
     expect(newState).to.have.property('loading', false)
-  });
+  })
 
   it('should handle RECIEVE_NODES action on initial state', () => {
     const action = actions.recieveNodes(nodes)
     const newState = reducer(undefined, action)
     expect(newState.get('nodes').toJS()).to.deep.equal(nodes)
-  });
+  })
+  /**
+   * UPDATE_NODE should update properties of current node
+   * and update node in 'nodes' array.
+   * This test tries to update 'rating' property of current node
+   * and checks that everything is updated.
+   */
+  it('should handle UPDATE_NODE action', () => {
+    // create state with nodes and apply action to it
+    const newState = reducer(
+      fromJS({nodes}),
+      actions.updateNode({rating: '2.232'})
+    ).toJS()
+    // find node to update in 'nodes' array
+    const nodeInNodesArray = newState.nodes.find(i => i.id == node.id)
+    // check if properties of current node are being updated
+    expect(newState).to.have.property('rating', '2.232')
+    // check that node in in array is updated
+    expect(nodeInNodesArray).to.have.property('id', node.id)
+    expect(nodeInNodesArray).to.have.property('rating', '2.232')
+  })
 
-  it('should handle UPDATE_NODE action', async () => {
-    expect(
-      reducer(undefined, actions.updateNode(node))
-    )
-    .to.have.property('id', node.id)
-  });
-
-  it('should handle TOGGLE_DIALOG action on initial state', async () => {
+  it('should handle TOGGLE_DIALOG action on initial state', () => {
     expect(
       reducer(undefined, actions.toggleDialog(true))
     )
     .to.have.property('dialogIsOpen', true)
-  });
+  })
 
   it('should handle UNLOAD_NODE action', () => {
     const action = actions.unloadNode()
     const newState = reducer(undefined, action)
     expect(newState).to.equal(initialState)
-  });
+  })
 
   it('should handle REMOVE_NODE action', () => {
     const action = actions.recieveNodes(nodes)
@@ -71,7 +85,7 @@ describe('user reducer', async () => {
     expect(newState.get('nodes').toJS())
       .to.have.length(2)
       .and.not.contain({id: 1})
-  });
+  })
 
   it('should handle RECIEVE_SEARCHED_VIDEOS action on initial state', () => {
     const action = actions.recieveSearchedVideos([])
@@ -81,6 +95,6 @@ describe('user reducer', async () => {
         searchIsActive: false,
     })
     expect(newState).to.deep.eq(expectedState)
-  });
+  })
 
-});
+})
